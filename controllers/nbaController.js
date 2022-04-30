93e2f88d047f9de69e6c47d9d9891fc48ee801e1
Charnaye Grier <charnayegrier@gmail.com>
2022-04-29 22:57:00 -0400

    all get routes working

--- controllers/nbaController.js
+++ controllers/nbaController.js
@@ -10,22 +10,42 @@ router.get('/', (req,res) => {
     .then((players) => res.render('index', {nbaPlayers: players}))
     .catch((err) => res.json(err))
 })
+//New route
+router.get('/new', (req,res) => {
+    res.render('new');
+    })
 
 // GET / read / players by college
 router.get('/:school', (req,res) => {
-const school = req.params.League_School
-    Nba.find({League_School: {$eq: school}})    
-    .then((players) => res.render('index', {nbaPlayers: players}))
+const school = req.params.school[0].toUpperCase() + req.params.school.slice(1)
+console.log(school)
+    Nba.find({League_School: school})  
+    .then((players) => {
+    console.log(players)
+    res.render('index', {nbaPlayers: players})})
+    .catch((err) => res.json(err))
 })
 
 //GET / read / players by position
-router.get('/:role', (req,res) => {
-    const role = req.params.Position
-    Nba.findById({Position: {$eq: role}})
-    .then((players) => res.render('index', {nbaPlayers: players}))
+router.get('/position/:role', (req,res) => {
+    const role = req.params.role.toUpperCase()
+    console.log(role)
+    Nba.find({Position: role})
+    .then((players) => { 
+    console.log(players)
+    res.render('index', {nbaPlayers: players})})
+    .catch((err) => res.json(err))
 })
-//POST / create / adding players to the database
 
+
+//POST / create / adding players to the database
+router.post('/', (req,res) => {
+    Nba.create(req.body)
+    .then((players) => {
+        res.redirect('/')
+    })
+    .catch((err) => res.json(err))
+})
 
 //PUT / update / players information
 
@@ -33,6 +53,8 @@ router.get('/:role', (req,res) => {
 
 
 //DELETE / delete player from the database
-
+router.delete('/:', (req,res) => {
+    Nba.findOneAndRemove({})
+})
 
 module.exports = router

4f6a99a0840a8310ec0c945d14ae5f6330ef95ca
Charnaye Grier <charnayegrier@gmail.com>
2022-04-29 23:41:12 -0400

    heroku attempt 2

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const mongoURI =
         ? process.env.DB_URL
         : DB_URL='mongodb+srv://charnaye95:naye95@cluster1.klor5.mongodb.net/DraftDay?retryWrites=true&w=majority';
         
-mongoose.connect(mongoURI)
+mongoose.connect(process.env.MONGODB_URI || mongoURI)
 .then(instance =>
     console.log(`Connected to db: ${instance.connections[0].name}`))
 .catch(error => console.log('Connection failed!', error));
